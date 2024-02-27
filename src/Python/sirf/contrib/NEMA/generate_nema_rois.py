3ba456c54dac7740c51836b9c277945df259189c
danieldeidda <nmdicom-recon@feijoa.npl.co.uk>
2024-02-27 10:20:12 +0000

    minor changes and clean up

--- src/Python/sirf/contrib/NEMA/generate_nema_rois.py
+++ src/Python/sirf/contrib/NEMA/generate_nema_rois.py
@@ -4,10 +4,10 @@ Usage:
   generate_nema_rois [--help | options]
 
 Options:
-  -s <file>, --sino=<file>     raw data file [default: no default you need an input of a NEMA sinogram]
+  -s <file>, --sino=<file>     raw data file ( no default you need an input of a NEMA sinogram)
   -o <out_path>, --outpath=<out_path>     path to data files, defaults to current directory
                                subfolder of SIRF root folder
-  -xy <xy_size>, --xysize=<xy_size> optional size of image in x and y, default is 150
+  -xy <xy_size>, --xysize=<xy_size> optional size of image in x and y 
 '''
 
 ## SyneRBI Synergistic Image Reconstruction Framework (SIRF)
@@ -47,9 +47,7 @@ import math as m
 from sirf.Utilities import show_2D_array, examples_data_path
 import sirf.STIR  as pet
 
-# process command-line option
-
-def recon_from_sino(acq_data, initial_image):
+def recon_from_sino(acq_data,image_size):
     
     # need to run a simple recon to register ROI with PET
     # using parallelproj
@@ -74,17 +72,16 @@ def recon_from_sino(acq_data, initial_image):
     recon.set_current_estimate(recon_im)
     # reconstruct
     recon.process()
-    recon.get_output()
     
     return recon.get_output() 
 
-def construct_NEMA_spheres_and_save(acq_data,image_size):
+def construct_NEMA_spheres_and_save(image):
     
     R=114/2
     z=140
     angle_smallest=210
     # create an empty image
-    empty_image = acq_data.create_uniform_image(0, xy=int(image_size[1]))
+    empty_image = image.get_uniform_copy(0)
     image=empty_image
     # assuming exagon shape
 
@@ -160,38 +157,38 @@ def construct_NEMA_spheres_and_save(acq_data,image_size):
 
     parfile=pet.get_STIR_examples_dir()+'/samples/stir_math_ITK_output_file_format.par'
 
-    image.write_par(data_output_path+'sphere.nii',parfile)
+    image.write_par(data_output_path+'unregistered_sphere.nii',parfile)
 
-    #spheres to nifty
-    image6.write_par(data_output_path+'sphere6.nii',parfile)
-    image5.write_par(data_output_path+'sphere5.nii',parfile)
-    image4.write_par(data_output_path+'sphere4.nii',parfile)
-    image3.write_par(data_output_path+'sphere3.nii',parfile)
-    image2.write_par(data_output_path+'sphere2.nii',parfile)
-    image1.write_par(data_output_path+'sphere1.nii',parfile)
+    #unregistered_spheres to nifty
+    image6.write_par(data_output_path+'unregistered_sphere6.nii',parfile)
+    image5.write_par(data_output_path+'unregistered_sphere5.nii',parfile)
+    image4.write_par(data_output_path+'unregistered_sphere4.nii',parfile)
+    image3.write_par(data_output_path+'unregistered_sphere3.nii',parfile)
+    image2.write_par(data_output_path+'unregistered_sphere2.nii',parfile)
+    image1.write_par(data_output_path+'unregistered_sphere1.nii',parfile)
 
 def do_registration(recon_image):
      
     parfile=pet.get_STIR_examples_dir()+'/samples/stir_math_ITK_output_file_format.par'
     recon_image.write_par(data_output_path+'recon.nii',parfile)
     recon_nii=Reg.NiftiImageData3D(data_output_path+'recon.nii')
-    spheres_nii=Reg.NiftiImageData3D(data_output_path+'sphere.nii')
-    sphere_nii= []
+    unregistered_spheres_nii=Reg.NiftiImageData3D(data_output_path+'unregistered_sphere.nii')
+    unregistered_sphere_nii= []
 
     for i in range(1,6+1):
-        sphere_nii.append(Reg.NiftiImageData3D(data_output_path+'sphere'+str(i)+'.nii'))
+        unregistered_sphere_nii.append(Reg.NiftiImageData3D(data_output_path+'unregistered_sphere'+str(i)+'.nii'))
 
     #now let's register
     # The following has a bug so we have used image1.write_par() to create nii images
     # osem_nii = Reg.NiftiImageData(osem_image)
-    # spheres_nii = Reg.NiftiImageData(image)
+    # unregistered_spheres_nii = Reg.NiftiImageData(image)
 
     # Set to NiftyF3dSym for non-rigid
     algo = Reg.NiftyAladinSym()
 
     # Set images
     algo.set_reference_image(recon_nii)
-    algo.set_floating_image(spheres_nii)
+    algo.set_floating_image(unregistered_spheres_nii)
     #set parameters
     algo.set_parameter('SetPerformRigid','1')
     algo.set_parameter('SetPerformAffine','0')
@@ -203,12 +200,12 @@ def do_registration(recon_image):
     np.set_printoptions(precision=3,suppress=True)
     TM = algo.get_transformation_matrix_forward()
     print(TM.as_array())
-    return TM, reg_image, sphere_nii
+    return TM, reg_image, unregistered_sphere_nii
 
 def generate_nema_rois(recon_image):
     
-    construct_NEMA_spheres_and_save(acq_data,image_size)
-    TM, reg_image, sphere_nii = do_registration(recon_image)    
+    construct_NEMA_spheres_and_save(recon_image)
+    TM, reg_image, unregistered_sphere_nii = do_registration(recon_image)    
 
     #once we have the registration matrix we can then apply it to the sphere generation
     resampler = Reg.NiftyResample()
@@ -218,18 +215,20 @@ def generate_nema_rois(recon_image):
     # Add the desired transformation to apply
     resampler.add_transformation(TM)
     resampler.set_padding_value(0)
-    # Use linear interpolation
+    # Use nearest neighbour interpolation
     resampler.set_interpolation_type_to_nearest_neighbour()
 
     for j in range(6):
     # Set image to resample
-        resampler.set_floating_image(sphere_nii[j])
+        resampler.set_floating_image(unregistered_sphere_nii[j])
     # Go!
         resampler.process()
         Roi = resampler.get_output()
         Roi.write(data_output_path+'S'+str(j+1)+'.nii')#TODO getting sirf imagedata to nifty to work without messing the orientation
         ROIsirf = pet.ImageData(data_output_path+'S'+str(j+1)+'.nii')
         ROIsirf.write('S'+str(j+1))
+
+    return ROIsirf
 
 if __name__ == '__main__':
     from docopt import docopt
@@ -246,8 +245,9 @@ if __name__ == '__main__':
 
     xy_size = args['--xysize']
     if xy_size is None:
-        print('Warning: I am setting x and y size to 150 make sure your reconstructed image has the same dimension')
-        xy_size=150
+        print('Warning: Setting image xy size to 150. Make sure your reconstructed image has the same dimension as the xy_size')
+        xy_size = 150
+
 
     acq_data = pet.AcquisitionData(sino_file)
     initial_image =  acq_data.create_uniform_image(1.0, xy=xy_size)
