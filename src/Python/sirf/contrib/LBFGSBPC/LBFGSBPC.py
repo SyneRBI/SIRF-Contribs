fd78865e64d8c3cea11dfe09cfcc769333f2ce15
Kris Thielemans <k.thielemans@ucl.ac.uk>
2025-10-28 14:44:39 +0000

    make loss increase and compare with restarting etc

--- src/Python/sirf/contrib/LBFGSBPC/LBFGSBPC.py
+++ src/Python/sirf/contrib/LBFGSBPC/LBFGSBPC.py
@@ -4,6 +4,7 @@
 # Class implementing the LBFGSB-PC algorithm in sirf.STIR
 #
 # Authors:  Kris Thielemans
+# Based on 
 #
 # Copyright 2025 University College London
 
@@ -17,10 +18,12 @@ class LBFGSBPC:
     ''' Implementation of the LBFGSB-PC Algorithm
     
     See
-    #sai et al,
+    Tsai et al,
     Fast Quasi-Newton Algorithms for Penalized Reconstruction in Emission Tomography and Further Improvements via Preconditioning
     IEEE TRANSACTIONS ON MEDICAL IMAGING, VOL. 37, NO. 4, APRIL 2018
     DOI: 10.1109/TMI.2017.2786865
+
+    WARNING: it maximises the objective function (as required by sirf.STIR.ObjectiveFunction).
     
     This implementation is NOT a CIL.Algorithm, but it behaves somewhat as one.
     '''
@@ -50,23 +53,28 @@ class LBFGSBPC:
 
     def callback(self, x):
         if self.update_objective_interval > 0 and self.iter % self.update_objective_interval == 0:
-            self.loss.append(self.precond_objfun_value(x))
+            self.loss.append(-self.precond_objfun_value(x))
             self.iterations.append(self.iter)
         self.iter += 1
 
-    def process(self) -> None:
+    def process(self, iterations=None) -> None:
+        if iterations is None:
+            raise ValueError("`run()` missing number of `iterations`")
         precond_init = self.initial / self.Dinv_SIRF
         self.trunc_filter.apply(precond_init)
         precond_init = precond_init.asarray().ravel()
         bounds = precond_init.size * [(0, None)]
         self.iter = 0
-        self.iterations = [0]
-        self.loss = [self.precond_objfun_value(precond_init)] if self.update_objective_interval > 0 else []
+        self.loss = []
+        self.iterations = []
+        # TODO not really required, but it differs from the first value reported by fmin_l_bfgs_b. Not sure why...
+        self.callback(precond_init)
+        self.iter = 0 # set back again
         res = fmin_l_bfgs_b(
             self.precond_objfun_value,
             precond_init,
             self.precond_objfun_gradient,
-            maxiter=50, # TODO use kwargs
+            maxiter=iterations,
             bounds=bounds,
             m=20,
             callback=self.callback,
@@ -80,8 +88,9 @@ class LBFGSBPC:
         #self.x.show(title='final image in preconditioned space')
         #self.x *= self.Dinv_SIRF
 
-    def run(self) -> None: # CIL alias, would need to support iterations keyword?
-        self.process()
+    def run(self, **kwargs) -> None: # CIL alias, would need to callback and verbose keywords etc
+        self.process(**kwargs)
         
     def get_output(self) -> STIR.ImageData:
         return self.x
+    
