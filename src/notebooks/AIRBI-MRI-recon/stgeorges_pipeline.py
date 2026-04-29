61c335c2324daa624cd7c86fc29a3dcdd0f253ef
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2026-04-29 08:09:44 +0000

    revert start gadgetron in python

--- src/notebooks/AIRBI-MRI-recon/stgeorges_pipeline.py
+++ src/notebooks/AIRBI-MRI-recon/stgeorges_pipeline.py
@@ -64,9 +64,6 @@ for fname in input_files:
     logger.info(f"Modified ISMRMRD file saved as {file_out_mod}")
     mod_input_files.append(file_out_mod)
 
-
-# start gadgetron
-gadgetron = subprocess.Popen(["gadgetron"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 # reconstruct AI recon
 
 for file in mod_input_files:
@@ -113,9 +110,6 @@ for file in mod_input_files:
             filename_prefix="sirf_recon_" + os.path.basename(file).replace(".h5", ""),
             series_description=f"SIRF recon_{num_iterations} LS+ {alpha} TV"
         )
-
-# stop gadgetron
-gadgetron.kill()
 
 # delete proc_dir
 if os.path.exists(proc_dir):
